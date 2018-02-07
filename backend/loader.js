54c9eb7f71fb6c15306ab96b7ea2c7dc2c946feb
Higor <Higor@HIGOR-PC>
2018-02-06 23:29:53 -0200

    Configurações Iniciais do Express

---
+++ backend/loader.js
@@ -0,0 +1,14 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const server = express()
+
+/* Middlewares responsáveis por realizarem o parser dos dados, tanto das submissões de formulários, quanto de objetos JSON. */
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)  
+})
