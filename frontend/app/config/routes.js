ab88d0ea2981882bdeec1fe1ca7e5a4b3c8119c6
Higor <higorbianchetti@gmail.com>
2018-02-16 18:19:58 -0200

    Configurações iniciais das rotas com UI Router.

---
+++ frontend/app/config/routes.js
@@ -0,0 +1,15 @@
+angular.module('primeiroApp').config([
+    '$stateProvider', 
+    '$urlRouterProvider', 
+    function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('dashboard', {
+            url: "/dashboard",
+            templateUrl: "dashboard/dashboard.html"
+        }).state('billingCycle', {
+            url: "/billingCycles",
+            templateUrl: "billingCycle/tabs.html"
+        })
+
+        $urlRouterProvider.otherwise('/dashboard')
+    }
+])
