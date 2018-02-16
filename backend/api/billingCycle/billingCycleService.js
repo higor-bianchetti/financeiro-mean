04fe435d4d2405540297a23a023a05bf5a65ad72
Higor <higorbianchetti@gmail.com>
2018-02-15 23:00:16 -0200

    Padronizando as mensagens de erro.

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const BillingCycle = require('./billingCycle')
 
 /* Cria uma API rest para os métodos get, post, put e delete. */
@@ -7,6 +8,28 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
 Para que a API retorne o objeto atual depois do update é necessário passar o valor new como true. 
 OBS: Por padrão, a API também não checa as validações em requisições PUT. Para que as requisições PUT também sejam validadas é preciso passar o valor runValidators: true. */
 BillingCycle.updateOptions({new: true, runValidators: true})
+
+/* Middleware que será chamado sempre após uma requisição POST ou PUT para padronizar as mensagens de erros. */
+BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+
+function sendErrorsOrNext(req, res, next) {
+    const bundle = res.locals.bundle
+
+    if(bundle.errors) {
+        var errors = parseErrors(bundle.errors)
+        res.status(500).json({errors})
+    } else {
+        next()
+    }
+}
+
+function parseErrors(nodeRestfulErrors) {
+    const errors = []
+
+    _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+
+    return errors
+}
 
 /* Criando uma rota que retorna a quantidade de objetos que estão no banco. */
 BillingCycle.route('count', function(req, res, next) {
