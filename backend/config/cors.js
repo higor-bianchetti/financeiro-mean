73460c62c9bb905f2670f48a3050dbeba475f25f
Higor <higorbianchetti@gmail.com>
2018-02-17 16:47:05 -0200

    Permite o acesso de outras aplicacções à API (CORS)

---
+++ backend/config/cors.js
@@ -0,0 +1,8 @@
+/* Permite que a API (backend) possa ser acessada por outra aplicação. */
+module.exports = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+
+    next()
+}
--- backend/config/server.js
+++ backend/config/server.js
@@ -2,12 +2,14 @@ const port = 3003
 
 const bodyParser = require('body-parser')
 const express = require('express')
+const allowCors = require('./cors')
 
 const server = express()
 
 /* Middlewares responsáveis por realizarem o parser dos dados, tanto das submissões de formulários, quanto de objetos JSON. */
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`)  
