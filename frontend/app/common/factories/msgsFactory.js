0721ab279b2cc62d70f6ac98ee8baecd45cabe88
Higor <higorbianchetti@gmail.com>
2018-02-18 14:44:21 -0300

    Criando uma factory para exibição de mensagens de sucesso e/ou erros.

---
+++ frontend/app/common/factories/msgsFactory.js
@@ -0,0 +1,26 @@
+(function() {
+    angular.module('primeiroApp').factory('msgs', [
+        'toastr',
+        MsgsFactory
+    ])
+
+    function MsgsFactory(toastr) {
+        function addMsg(msgs, title, method) {
+            if(msgs instanceof Array) {
+                msgs.forEach(msg => toastr[method](msg, title))
+            } else {
+                toastr[method](msgs, title)
+            }
+        }
+
+        function addSuccess(msgs) {
+            addMsg(msgs, 'Sucesso', 'success')
+        }
+
+        function addError(msgs) {
+            addMsg(msgs, 'Erro', 'error')
+        }
+
+        return {addSuccess, addError}
+    }
+})()
