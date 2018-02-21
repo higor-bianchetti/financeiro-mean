833aecfa7ccf1580e3f76b7c9abaf01e01fcfdae
Higor <higorbianchetti@gmail.com>
2018-02-21 15:07:42 -0300

    Definindo as rotas que serão protegidas pelo Token JWT.

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,14 +1,26 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
+    // Rotas abertas
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signUp)
+    openApi.post('/validateToken', AuthService.validateToken)
+
+    // Rotas protegidas por Token JWT
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
+
     // API de rotas
-    const router = express.Router()
-
-    server.use('/api', router)
-
     const billingCycleService = require('../api/billingCycle/billingCycleService')
-    billingCycleService.register(router, '/billingCycles')
+    billingCycleService.register(protectedApi, '/billingCycles')
 
     const billingSummaryService = require('../api/billingSummary/billingSummaryService')
-    router.route('/billingSummary').get(billingSummaryService.getSummary)
+    protectedApi.route('/billingSummary').get(billingSummaryService.getSummary)
 }
